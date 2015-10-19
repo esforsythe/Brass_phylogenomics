c90a4121b416eec8456da11ac679e7177f65144a
Evan Forsythe <esforsythe@email.arizona.edu>
2015-10-19 14:51:54 -0700

    I added a comment line to my script.

--- src/eric_parse.pl
+++ src/eric_parse.pl
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#To use this script, generate a listfile of genes you would like to keep (singletons).  These genes will be parsed  
 while read p; do
   #echo $p
   grep -A 1 $p Athal.fasta >>Athal_singletons.fasta
